474d3491128971a044fe96955d8cfc8f5bd7d993
Lina Teckentrup <46699837+lteckentrup@users.noreply.github.com>
2021-04-19 14:25:21 +1000

    Update qdm.py

--- qdm.py
+++ qdm.py
@@ -4,33 +4,36 @@ from scipy.stats.mstats import mquantiles
 from scipy.interpolate import interp1d
 
 def qdm(var, obs, pred, cor, threshold, nquantiles):
-    if (np.isnan(obs).all()==True) and (np.isnan(pred).any()==True) and (np.isnan(cor).any()==True):
+
+    eps = np.finfo(float).eps
+
+    if (np.isnan(obs).all()==True and np.isnan(pred).any()==True and
+        np.isnan(cor).any()==True):
         yout = itertools(np.nan, len(cor))
 
     else:
-        # Apply a small amount of jitter to accomodate ties due to limited measurement precision -> ignore for now
-        #o <- jitter(o, jitter.factor)
-        #p <- jitter(p, jitter.factor)
-        #s <- jitter(s, jitter.factor)
-
-        # For ratio data, treat exact zeros as left censored values less than pr.threshold
+        ### For ratio data, treat exact zeros as left censored values less than
+        ### threshold. Original R script adds jitter; ignore
         if var == 'prec':
-            obs[np.where((obs<2) & (obs!=np.nan))] = np.random.uniform(low=0,
-                                                                       high=threshold,
-                                                                       size=np.nansum(obs<threshold))
-            pred[np.where((pred<2) & (pred!=np.nan))] = np.random.uniform(low=0,
-                                                                          high=threshold,
-                                                                          size=np.nansum(pred<threshold))
-            cor[np.where((cor<2) & (cor!=np.nan))] = np.random.uniform(low=0,
-                                                                       high=threshold,
-                                                                       size=np.nansum(cor<threshold))
+            obs[np.where((obs<threshold) & (obs!=np.nan))] = \
+            np.random.uniform(low=eps, high=threshold,
+                              size=np.nansum(obs<threshold))
+            pred[np.where((pred<threshold) & (pred!=np.nan))] = \
+            np.random.uniform(low=eps, high=threshold,
+                              size=np.nansum(pred<threshold))
+            cor[np.where((cor<threshold) & (cor!=np.nan))] = \
+            np.random.uniform(low=eps, high=threshold,
+                              size=np.nansum(cor<threshold))
 
         # Calculate empirical quantiles using Weibull plotting position
         n = max(len(obs), len(pred), len(cor))
-        if nquantiles == 0:
-            nquantiles = n
+        if nquantiles == None or nquantiles == 0:
+            nquant = n
+        else:
+            nquant = nquantiles
 
-        tau = (np.linspace(1./(n+1), n/(n+1), num=12))
+        tau = (np.linspace(1./(n+1), n/(n+1), num=nquant))
+
         ## precision of mquantile??
         quantobs = mquantiles(obs, prob = tau, alphap=0, betap=0)
         quantpred = mquantiles(pred, prob = tau, alphap=0, betap=0)
@@ -42,9 +45,9 @@ def qdm(var, obs, pred, cor, threshold, nquantiles):
 
         if var == 'prec':
             p2o_pred = interp1d(tau, quantpred, kind='linear', bounds_error=False)
-            delta = cor/p2o_pred(taucor)
+            delta = cor/(p2o_pred(taucor)+np.finfo(float).eps)
             p2o_yout = interp1d(tau, quantobs)
-            yout = p2o_yout(taucor)
+            yout = p2o_yout(taucor)*delta
         else:
             p2o_pred = interp1d(tau, quantpred, kind='linear', bounds_error=False)
             delta = cor - p2o_pred(taucor)
